305bb94e99474797576a003d8b38a8cc3a949ac4
tmpted <110581818+ppakkk@users.noreply.github.com>
2022-08-11 16:51:41 +0900

    update

---
+++ source.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+
+}
+
+//파일입출력 함수
+/*
+void open_noun()
+{
+	FILE* fp = fopen("명사.txt", "r");
+
+	fclose(fp);
+
+	return 0;
+}
+
+void open_adjective()
+{
+	FILE* fp = fopen("형용사.txt", "r");
+	fclose(fp);
+
+	return 0;
+}
+*/
+
+void rand_arr()
+{
+
+}
